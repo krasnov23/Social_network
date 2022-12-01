a06061df70820232bf0d4c1b8a0db3777e63eb93
Boris Krasnov <crasnov.boris@yandex.ru>
2022-12-02 00:56:41 +0300

    Add simple form

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,7 +16,7 @@ class MicroPostController extends AbstractController
     public function index(MicroPostRepository $posts): Response
     {
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $posts->findAll(),
         ]);
     }
 
@@ -23,14 +24,36 @@ class MicroPostController extends AbstractController
     #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
     public function showOne(MicroPost $post): Response
     {
-        // Команда для упрощения работы с выделенным айди
-        // composer require sensio/framework-extra-bundle
-        dd($post);
-        //return $this->render('micro');
-        // https://symfony.com/bundles/SensioFrameworkExtraBundle/current/index.html
-        // Также мы можем указать любое из свойств класса и по введению в адрес имени этого свойства будет выведен нужный нам объект
-        // Как и по id (Например: title)
+
+        return $this->render('micro_post/show.html.twig',
+        [ 'post' => $post ]);
+
     }
+
+    #[Route('/micro-post/add', name: 'app_micro_post_add',priority: 2)]
+    public function add(): Response
+    {
+        $microPost = new MicroPost();
+        // Подготовка класса к созданию формы, добавление полей поля должны соответствовать свойствам класса
+        $form = $this->createFormBuilder($microPost)
+            ->add('title')->add('text')->add('submit',SubmitType::class,
+                ['label' => 'Save'])->getForm();
+        // label - атрибут обозначения кнопки submit
+
+        // Передача формы в шаблон, в шаблоне переменная form вставляет в функцию для форм form
+        return $this->renderForm("micro_post/add.html.twig",
+        ["forma" => $form]);
+
+
+    }
+
+    // Команда для упрощения работы с выделенным айди
+    // composer require sensio/framework-extra-bundle
+
+    //return $this->render('micro');
+    // https://symfony.com/bundles/SensioFrameworkExtraBundle/current/index.html
+    // Также мы можем указать любое из свойств класса и по введению в адрес имени этого свойства будет выведен нужный нам объект
+    // Как и по id (Например: title)
 
     // Находит все записи в таблице
     //dd($posts->findall());
